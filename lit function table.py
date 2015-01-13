6e3355ec8a64afd1fb8fdf841856c381ca450ba1
40756 <40756@PC004143.coll.lr.local>
2015-01-13 11:10:57 +0000

    table function

---
+++ lit function table.py
@@ -0,0 +1,14 @@
+##def asklist():
+##    lists = int(input("how many lists would you like: "))
+##    for thing in range(lists):
+##        
+##    
+##    items = int(input("please eneter how many items will be in your list: "))
+##    table = []
+##
+##    for question in range(items):
+##        answer = input("what item would you like to add: ")
+##        table.append(answer)
+##
+##    print(table)
+##asklist()
